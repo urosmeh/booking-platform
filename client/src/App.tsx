03cd8d0b4d638a896a9dc3b09586f91ebd19f311
Uroš Meh <uros.meh@pro-bit.si>
2023-06-04 17:38:31 +0200

    display salon names from api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 // import "./App.css";
 import { useGetSalonsQuery } from "./store";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,14 +10,15 @@ function App() {
 
   return (
     <>
-    {data && <div>{data[0].address}</div>}
-    test
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Box bg="brand.100">
+        <Flex direction={["column", "row"]}>
+          <Box>
+            {data?.map((salon) => (
+              <Box>{salon.name}</Box>
+            ))}
+          </Box>
+        </Flex>
+      </Box>
     </>
   );
 }
